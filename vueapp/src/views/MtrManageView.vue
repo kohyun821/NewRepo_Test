1a885fdc7e34b1a25a4361b936f0bd44d08672ae
hmj71 <hmj71@DESKTOP-79M6NVF>
2023-05-11 16:32:48 +0900

    my

--- vueapp/src/views/MtrManageView.vue
+++ vueapp/src/views/MtrManageView.vue
@@ -1,5 +1,80 @@
 <template>
     <div class="about">
         <h1>자재관리</h1>
+        <table v-if="materials.length">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>이름</th>
+                    <th>남은 양</th>
+                    <th>총 합</th>
+                    <th>상태</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="material in materials" :key="material.materialId">
+                    <td>{{ material.materialId }}</td>
+                    <td>{{ material.materialName }}</td>
+                    <td>{{ material.materialAmount }}</td>
+                    <td>{{ material.materialTotal }}</td>
+                    <td>{{ material.materialStatus ? '활성화' : '비 활성화' }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <div v-else>
+            <p>No materials found.</p>
+        </div>
+    </div>
+    <h1>
+        1515asdf
+    </h1>
+    <div>
+        <form @submit.prevent="CreateMaterial">
+            <div>
+                <label>이름</label>
+                <input type="text" id="materialName" v-model="FormData.materialName" />
+            </div>
+            <div>
+                <label>수량</label>
+                <input type="number" id="materialAmount" v-model="FormData.materialAmount" />
+            </div>
+            <div>
+                <label>합계</label>
+                <input type="number" id="materialTotal" v-model="FormData.materialTotal" />
+            </div>
+            <button>생성</button>
+        </form>
+
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'CreateMaterial',
+    data() {
+        return {
+            materials: [],
+            FormData: {
+                materialName: '',
+                materialAmount: '',
+                materialTotal: ''
+            }
+        }
+    },
+    async created() {
+        try {
+            const response = await axios.get('https://localhost:7059/Material')
+            console.log(response.data);
+            this.materials = response.data
+        } catch (error) {
+            console.error(error)
+        }
+    },
+    methods:{
+        CreateMaterial(){
+            axios.post("")
+        }
+    },
+}
+</script>
--- webapi/Controllers/DepartmentController.cs
+++ webapi/Controllers/DepartmentController.cs
@@ -4,6 +4,8 @@ using webapi.Models;
 
 namespace webapi.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class DepartmentController : Controller
     {
 
@@ -12,21 +14,86 @@ namespace webapi.Controllers
 
         public DepartmentController(MyWorldDbContext _mWorldDbContext)
         {
-            _DBmWorldDbContext = _mWorldDbContext;
+            this._DBmWorldDbContext = _mWorldDbContext;
         }
 
+        [HttpPost]
+        [Route("RegistDepartment")]
+        public async Task<IActionResult> RegistDepartment([FromBody] Department department)
+        {
+            try
+            {
+                //같은 이름으로 생성 불가능 하도록 
+                var dbDepartment = _DBmWorldDbContext.Departments.Where(u => u.DepartmentName == department.DepartmentName).FirstOrDefault();
+                if (dbDepartment != null)
+                {
+                    return BadRequest("Fail");
+                }
+                //department.DepartmentStatus = true;
+                _DBmWorldDbContext.Departments.Add(department);
+                await _DBmWorldDbContext.SaveChangesAsync();
+
+                return Ok("Success");
+
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+
+        }
         [HttpGet]
-        [Route("Department")]
         public async Task<IActionResult> GetDepartment()
         {
             try
             {
-                List<Department> listdepartment = _DBmWorldDbContext.Departments.ToList();
-                if (listdepartment != null)
+                var departments = _DBmWorldDbContext.Departments
+                              .ToList();
+
+                if (!departments.Any())
                 {
-                    return Ok(listdepartment);
+                    return NotFound("Department not found");
                 }
-                return Ok("No Database");
+                return Ok(departments);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("ById/{id}")]
+        public async Task<IActionResult> GetDepartmentById(int id)
+        {
+            try
+            {
+                var department = await _DBmWorldDbContext.Departments.FindAsync(id);
+                if (department == null)
+                {
+                    return NotFound("Department not found");
+                }
+                return Ok(department);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("ByName/{departmentName}")]
+        public async Task<IActionResult> GetDepartmentByName(string departmentName)
+        {
+            try
+            {
+                //여러개 리턴 할 수 있도록 department 가 아닌 departments로 변수명 정함..
+                var departments = _DBmWorldDbContext.Departments
+                               .Where(d => d.DepartmentName == departmentName)
+                               .ToList();
+
+                if (!departments.Any())
+                {
+                    return NotFound("Department not found");
+                }
+                return Ok(departments);
             }
             catch (Exception ex)
             {
