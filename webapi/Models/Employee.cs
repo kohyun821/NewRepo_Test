a237f99cfbe673c4d693485846abb6511a1d3e0d
도현 김 <dhkim7750@yullin.com>
2023-05-11 16:22:10 +0900

    세팅 완료?

--- webapi/Models/Employee.cs
+++ webapi/Models/Employee.cs
@@ -7,11 +7,9 @@ public partial class Employee
 {
     public int EmployeeId { get; set; }
 
-    public string? EmployeeName { get; set; }
+    public string EmployeeName { get; set; } = null!;
 
     public int DepartmentId { get; set; }
-
-    public string? EmployeePhone { get; set; }
 
     public virtual Department Department { get; set; } = null!;
 
