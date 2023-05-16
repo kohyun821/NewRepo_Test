93f07a7d8badfbd6a4b2695e967118fb03b73220
도현 김 <dhkim7750@yullin.com>
2023-05-16 16:38:50 +0900

    Pull 후 push

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,66 +1,49 @@
 <template>
-    <PageHeader></PageHeader>
-    <router-view></router-view>
-    <PageFooter></PageFooter>
-    <button @click="get">데이터 가져오기</button>
-    <p>{{ data }}</p>
+  <PageHeader></PageHeader>
+  <router-view></router-view>
+  <PageFooter></PageFooter>
 </template>
 
 <script>
-  import PageHeader from "./components/PageHeader.vue"
-  import PageFooter from "./components/PageFooter.vue"
+import PageHeader from "./components/PageHeader.vue"
+import PageFooter from "./components/PageFooter.vue"
 
-  export default {
-    name:"App",
-    components: {
-      PageFooter,
-      PageHeader
-    },
-    data() {
-      return {
-        data : [],
-        id : 1
-      }
-    },
-    methods: {
-      get(){
-        this.axios
-        .get(`http://localhost:54884/api/department/depList`)
-        .then((result)=>{
-          console.log(result.data);
-          this.data.push(result.data);
-          this.id++;
-        })
-        .catch((error)=>{
-          console.log(error);
-        })
-        .finally(()=>{
-          console.log("finally");
-        });
-      }
+export default {
+  name: "App",
+  components: {
+    PageFooter,
+    PageHeader
+  },
+  data() {
+    return {
+
     }
+  },
+  methods: {
+
   }
+}
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-    #nav {
-        padding: 30px;
-    }
+#nav {
+  padding: 30px;
+}
 
-        #nav a {
-            font-weight: bold;
-            color: #2c3e50;
-        }
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-            #nav a.router-link-exact-active {
-                color: #42b983;
-            }
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
